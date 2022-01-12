99512466b1caad0a0f67e30c7389da77cb398f56
Noah Taggart <noahtag@gmail.com>
2022-01-12 10:13:45 -0600

    created a function to quarter an integer

---
+++ main.js
@@ -0,0 +1,13 @@
+const quarterValue = (mathNumber) => {
+    const quarter = mathNumber / 4
+
+    return quarter
+
+
+}
+
+const returnedNumber = quarterValue(16)
+
+
+// Log the return value
+console.log(returnedNumber)
